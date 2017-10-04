a0166ea5cdd0e90fa372b50f20670cb6ed32dd89
Lance Liu <lanceknight@users.noreply.github.com>
2017-10-04 14:34:48 -0500

    2 features
    there are two features now: size(x1), month(x2)

--- initialization.py
+++ initialization.py
@@ -1,8 +1,10 @@
 import numpy as np
 
 def getX_y():
-    file_dir = "data/Processed Data.csv"
+    file_dir = "data/Processed Data2.csv"
     y = np.array([])
+    x1 = np.array([])
+    x2 = np.array([])
     X = np.array([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])
     #get data and put them into X, y respectively
     count = 0
@@ -12,9 +14,13 @@ def getX_y():
             count +=1
             #print line
             contents = line.split(",")
-            numbers = contents[2].split("\n")
-            number = float(numbers[0])
-            y = np.append(y,number)
-    X = np.array(range(1,count+1))
+            input1 = float(contents[0])
+            input2 = float(contents[1])
+            #outputs = contents[2].split("\n")
+            output = float(contents[2])
+            x1 = np.append(x1, input1)
+            x2 = np.append(x2, input2)
+            y = np.append(y,output)
+    #X = np.array(range(1,count+1))
 
-    return X,y
+    return x1,x2,y
--- main.py
+++ main.py
@@ -17,8 +17,8 @@ theta = np.array([0.1, 0.2, 0.3])
 
 
 #get X, y as numpy array
-(X,y) = getX_y()
-X = process_X(X)
+(x1,x2, y) = getX_y()
+X = process_X(x1, x2)
 # print "shape(X):", np.shape(X)
 # print "shape(theta):", np.shape(theta)
 # print h(X,theta)
@@ -36,9 +36,9 @@ plt.figure()
 plt.title("Sales Prediction")
 plt.xlabel("month(from Jun, 2016 to Sept,2017)")
 plt.ylabel("sales amount")
-plt.plot(X[:,1],y,'ro')
-plt.plot(X[:,1],h,'b')
-plt.show()
+plt.plot(X[:,2],y,'ro')
+plt.plot(X[:,2],h,'b')
+#plt.show()
 
 mpl.rcParams['legend.fontsize'] = 10
 
@@ -47,7 +47,11 @@ ax = fig.gca(projection='3d')
 
 x1 = X[:,1]
 x2 = X[:,2]
-ax.plot(x1, x2, y, label='parametric curve')
+ax.plot_wireframe(x1,x2, h, rstride=10, cstride=10)
+ax.scatter(x1, x2, y, label = "test", c = 'r')
+ax.set_xlabel('Size')
+ax.set_ylabel('Month')
+ax.set_zlabel('Sales Qty(SF)')
 ax.legend()
 
 plt.show()
